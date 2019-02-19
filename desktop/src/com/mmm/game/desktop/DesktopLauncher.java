8d00d940cf5e4e2c3e8b06c1e83ba2ce74034bf7
unknown <vietvh1995@gmail.com>
2019-02-19 09:51:04 +0700

    create project

---
+++ desktop/src/com/mmm/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mmm.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mmm.game.Snake2DGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Snake2DGame(), config);
+	}
+}
